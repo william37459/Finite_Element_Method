03f385af2fcf2260a42a30746687ca868810ce9a
Engberg <physicace2@gmail.com>
2024-05-06 15:20:40 +0200

    Lavede om til function for convergence rate

--- test.py
+++ test.py
@@ -6,48 +6,52 @@ from mpi4py import MPI
 import pyvista as pv
 import dolfinx.fem.petsc
  
- 
-## Define domain and functionspace over it
-NElem   = 16
-domain = mesh.create_rectangle(
-    comm=MPI.COMM_WORLD,
-    points=((-1.0, -1.0), (1.0, 1.0)),
-    n=(NElem, NElem)
-)
-V = fem.functionspace(domain, ("Lagrange", 2))
+def u_ex(mod):
+    return lambda x: mod.exp(x[0]+x[1])*mod.cos(x[0])*mod.sin(x[1])+x[0]
+u_ufl = u_ex(ufl)
+u_numpy = u_ex(np)
 
-## Define trial and test functions
-u = ufl.TrialFunction(V)
-v = ufl.TestFunction(V)
+def solver(N=10, degree=2):
+    ## Define domain and functionspace over it
+    domain = mesh.create_rectangle(
+        comm=MPI.COMM_WORLD,
+        points=((-1.0, -1.0), (1.0, 1.0)),
+        n=(N, N)
+    )
+    V = fem.functionspace(domain, ("Lagrange", degree))
+
+    ## Define trial and test functions
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    
+    ## Source term of the poisson equation
+    x = ufl.SpatialCoordinate(domain)
+    f = -ufl.div(ufl.grad(u_ufl(x)))
+    
+    ## Applying boundary conditions
+    uD      = fem.Function(V)
+    uD.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2)
+    tdim    = domain.topology.dim
+    fdim    = tdim-1
+    domain.topology.create_connectivity(fdim, tdim)
+    boundary_facets = mesh.exterior_facet_indices(domain.topology)
+    boundary_dofs   = fem.locate_dofs_topological(V, fdim, boundary_facets)
+    bc = fem.dirichletbc(default_scalar_type(0), boundary_dofs, V)
+    bc      = fem.dirichletbc(uD, boundary_dofs)
+    
+    # bilinear form
+    a       = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
+    L       = f * v * ufl.dx
+    
+    # set PETSc solver options
+    sol_opts = {"ksp_type": "preonly", "pc_type": "lu"}
+    # formulate the problem
+    problem = dolfinx.fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options=sol_opts)
+    return problem.solve(), u_ufl(x), domain, V, tdim
+
+uh, u_ex, domain, V, tdim = solver()
  
-#E Source term of the poisson equation
-x = ufl.SpatialCoordinate(domain)
-f = ufl.exp(x[0]+x[1])*ufl.classes.Cos(x[0])*ufl.classes.Sin(x[1]*x[0])+x[1]
  
-## Applying boundary conditions
-uD      = fem.Function(V)
-uD.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2)
-tdim    = domain.topology.dim
-fdim    = tdim-1
-domain.topology.create_connectivity(fdim, tdim)
-boundary_facets = mesh.exterior_facet_indices(domain.topology)
-boundary_dofs   = fem.locate_dofs_topological(V, fdim, boundary_facets)
-bc = fem.dirichletbc(default_scalar_type(0), boundary_dofs, V)
-bc      = fem.dirichletbc(uD, boundary_dofs)
- 
-# bilinear form
-a       = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
-L       = f * v * ufl.dx
- 
-# set PETSc solver options
-sol_opts = {"ksp_type": "preonly", "pc_type": "lu"}
-# formulate the problem
-problem = dolfinx.fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options=sol_opts)
-# solve the problem
-uh = problem.solve()
- 
- 
-# 2D contour plots of the mesh and result using pyvista
 pv.off_screen = True
 topology, cell_types, geometry = dolfinx.plot.vtk_mesh(domain, tdim)
 grid = pv.UnstructuredGrid(topology, cell_types, geometry)
